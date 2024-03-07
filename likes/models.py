855a5ab1e407d38e0a882418b0f7a2558d79c546
simope <simonepecora@live.it>
2024-03-07 21:28:43 +0100

    finished ORM section

--- likes/models.py
+++ likes/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
+
 class LikedItem(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.contenttypes.models import ContentType
+from django.db.models import Value, F
+from store.models import Product, Collection, Cart, CartItem
+from tags.models import TaggedItem
 
 def say_hello(request):
-    return render(request, 'hello.html', {'name': 'Simope'})
+
+    return render(request, 'hello.html', {'name':'Mosh'})

f5fb9653b5a73c592eebbdbf29ababe461c2b919
simope <simonepecora@live.it>
2024-03-09 15:14:45 +0100

    finished basic api section

--- store/urls.py
+++ store/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 # URLConf
 urlpatterns = [
-    path('products/', views.product_list)
+    path('products/', views.product_list),
+    path('products/<int:id>/', views.product_detail),
+    path('collections/', views.collection_list),
+    path('collections/<int:pk>/', views.collection_detail, name='collection-detail')
 ]
